40ac3878213f14d7220dc75e5d4dcb3ba408f271
APasu123coder <apcp2420@gmail.com>
2025-07-04 14:09:56 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Upload from "./pages/Upload";
-import Search from "./pages/Search";
+import SearchPage from "./pages/SearchPage";
 import Settings from "./pages/Settings";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
@@ -74,7 +74,7 @@ function AppContent() {
                     <Routes>
                       <Route path="/" element={<Dashboard />} />
                       <Route path="/upload" element={<Upload />} />
-                      <Route path="/search" element={<Search />} />
+                      <Route path="/search" element={<SearchPage />} />
                       <Route path="/settings" element={<Settings />} />
                       <Route path="*" element={<NotFound />} />
                     </Routes>
